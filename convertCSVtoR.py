7e55932c7bcd381337d071b967350c8da48c2767
Silvio Donato <silvio.donato@cern.ch>
2021-04-12 18:50:03 +0200

    Apr12

--- convertCSVtoR.py
+++ convertCSVtoR.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 import sys
+from datetime import datetime
 
 fileName = sys.argv[1]
 print(fileName)
@@ -14,7 +15,10 @@ for l in file_.readlines():
         value = value.replace("\n","")
         value = value.replace("<","")
         if ("/2021" in data1 or "/2020" in data1) and ("/2021" in data2 or "/2020" in data2):
+            dd1, mm1, yyyy1 = data1.split("/")
             dd, mm, yyyy = data2.split("/")
+            if int(yyyy)<=2020 and int(mm)<=2: continue
+            if ((datetime(int(yyyy),int(mm),int(dd)) - datetime(int(yyyy1),int(mm1),int(dd1))).days)>=-2: continue
             nl = "%s-%s-%s %s 0\n"%(yyyy, mm, dd, value)
             newfile.write(nl)
 
